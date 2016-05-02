750e05c20fc9b04afaa7c88911276a8407f00b92
Joel Edwards <joeledwards@gmail.com>
2016-05-02 01:04:37 -0600

    Added magnetic door sensor to monitor.js Added messages.js to monitor for alert messages.

---
+++ messages.js
@@ -0,0 +1,46 @@
+require('log-a-log');
+
+const _ = require('lodash');
+const Q = require('q');
+const moment = require('moment');
+const durations = require('durations');
+const cogs = require('cogs-sdk');
+
+var namespace = 'auto-monitor';
+var vehicleUuid = 'deadbeef-dead-beef-dead-beefdeadbeef';
+var attributes = {
+  'vehicle-uuid': vehicleUuid
+};
+
+var cogsClient;
+cogs.api.getClient('cogs.json')
+.then((client) => {
+  var d = Q.defer();
+  cogsClient = client;
+
+  console.log("Subscribing to topic...");
+
+  cogsClient.subscribe(namespace, attributes)
+  .then((ws) => {
+    console.log("Subscription established.");
+
+    ws.on('error', (error) => console.error(`Error with WebSocket connection: ${error}\n${error.stack}`));
+    ws.on('open', () => console.log(`WebSocket connection established.`));
+    ws.on('message', (json) => {
+      var message = JSON.parse(json);
+      console.log(`message: ${message.notification_msg}`)
+    });
+    //ws.on('acked', (messageId) => console.log(`message '${messageId}' acknowledged`));
+    ws.on('close', () => console.log(`closed`));
+    ws.on('reconnect', () => console.log(`WebSocket reconnected.`));
+  });
+
+  return d.promise;
+})
+.catch((error) => {
+  console.error(`Error with Cogs connection: ${error}\n${error.stack}`);
+})
+.finally(() => {
+  console.log("Done.");
+});
+
--- monitor.js
+++ monitor.js
@@ -7,33 +7,104 @@ const moment = require('moment');
 const durations = require('durations');
 const cogs = require('cogs-sdk');
 
-var pin = 22; // Signal pin for mercury switch (on == false, off = true)
+var doorSensorPin = 32; // Signal pin for magnetic switch (on == true, off = false)
+var hoodSensorPin = 22; // Signal pin for mercury switch (on == false, off = true)
 
 var stop = false;
 process.on('SIGINT', () => stop = true);
 
+// Cogs API client
+var cogsClient;
+function getCogsClient() {
+  if (cogsClient == null) {
+    return cogs.api.getClient('cogs.json')
+        .then((client) => {
+          cogsClient = client;
+          return cogsClient;
+        });
+  }
+  else {
+    return Q(cogsClient);
+  }
+}
+
+var namespace = 'auto-monitor';
+var vehicleUuid = 'deadbeef-dead-beef-dead-beefdeadbeef';
+
+// Driver door monitor.
+var driverDoorOpen = false;
+function doorSensorLoop() {
+  pi.get(doorSensorPin)
+  .then((value) => {
+    var newOpenValue = value;
+    var now = moment().valueOf();
+
+    if (newOpenValue != driverDoorOpen) {
+      lastStatusEvent = now;
+      console.log(`Sending door status update event: driver-door-open=${newOpenValue}`);
+      getCogsClient().then((client) => {
+        var eventName = `driver-door-${newOpenValue ? 'opened' :  'closed'}-${moment().toISOString()}`
+        var attributes = {
+          'vehicle-uuid': vehicleUuid,
+          'driver-door-open': newOpenValue
+        };
+
+        client.sendEvent(namespace, eventName, attributes)
+        .then(() => console.log(`Sent status update event.`))
+        .catch((error) => console.error(`Error deliverying status update event: ${error}\n${error.stack}`));
+      });
+    }
+
+    driverDoorOpen = newOpenValue;
+    console.log(`Driver door is ${driverDoorOpen ? 'open' : 'closed'}`);
+  })
+  .catch((error) => {
+    console.error(`Error reading pin ${doorSensorPin}: ${error}\n${error.stack}`);
+  })
+  .finally(() => {
+    if (stop === true) {
+      pi.shutdown().then(() => process.exit(0));
+    } else {
+      setTimeout(() => doorSensorLoop(), 500);
+    }
+  })
+}
+
+// Hood monitor.
 var hoodOpen = true;
 function hoodSensorLoop() {
-  pi.get(pin)
+  pi.get(hoodSensorPin)
   .then((value) => {
     var newOpenValue = !value;
+    var now = moment().valueOf();
 
     if (newOpenValue != hoodOpen) {
-      console.log("Sending hood status update event: hood-open=${newOpenValue}");
-      // TODO: send update
+      lastStatusEvent = now;
+      console.log(`Sending hood status update event: hood-open=${newOpenValue}`);
+      getCogsClient().then((client) => {
+        var eventName = `hood-${newOpenValue ? 'opened' :  'closed'}-${moment().toISOString()}`
+        var attributes = {
+          'vehicle-uuid': vehicleUuid,
+          'hood-open': newOpenValue
+        };
+
+        client.sendEvent(namespace, eventName, attributes)
+        .then(() => console.log(`Sent status update event.`))
+        .catch((error) => console.error(`Error deliverying status update event: ${error}\n${error.stack}`));
+      });
     }
 
     hoodOpen = newOpenValue;
     console.log(`Hood is ${hoodOpen ? 'open' : 'closed'}`);
   })
   .catch((error) => {
-    console.error(`Error reading pin ${pin}: ${error}\n${error.stack}`);
+    console.error(`Error reading pin ${hoodSensorPin}: ${error}\n${error.stack}`);
   })
   .finally(() => {
     if (stop === true) {
       pi.shutdown().then(() => process.exit(0));
     } else {
-      setTimeout(() => hoodSensorLoop(), 100);
+      setTimeout(() => hoodSensorLoop(), 500);
     }
   })
 }
@@ -41,6 +112,7 @@ function hoodSensorLoop() {
 // Program entry point.
 function monitor() {
   hoodSensorLoop();
+  doorSensorLoop();
 }
 
 monitor();
